05d94819de49ce99bccf919cdbff4d2811f47a27
cakl <sebastian.bock@bluewin.ch>
2015-02-26 17:34:45 +0100

    AF added

--- E2/analytischeFunktionen.sql
+++ E2/analytischeFunktionen.sql
@@ -1,1 +1,34 @@
-select * form dimmovie;
+select
+  dimmovie.subgenre,
+  sum(factsales.QUANTITY) as anzsales,
+  RANK() OVER (ORDER BY sum(factsales.quantity) ASC) rang
+from
+  factsales,
+  dimmovie
+where
+  factsales.movieid = dimmovie.movieid
+group by
+  dimmovie.subgenre
+;
+
+select
+  dimmovie.subgenre,
+  dimcustomer.gender,
+  sum(factsales.quantity),
+  GROUPING_ID(dimmovie.subgenre, dimcustomer.gender) groupingid,
+  RANK() OVER(PARTITION BY GROUPING_ID(dimmovie.subgenre, dimcustomer.gender) ORDER BY SUM(factsales.QUANTITY) DESC) as rank
+from
+  factsales,
+  dimmovie,
+  dimcustomer
+where
+  factsales.movieid = dimmovie.movieid
+  and
+  factsales.custid = dimcustomer.custid
+group by 
+  cube(dimmovie.subgenre, dimcustomer.gender)
+order by
+  dimmovie.subgenre
+;
+
+
