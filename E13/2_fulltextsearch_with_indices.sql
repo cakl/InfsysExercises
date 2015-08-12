3c3945f10151d32b37b4961a2bd5f8904fc909ff
cakl <sebastian.bock@bluewin.ch>
2015-08-12 21:51:22 +0200

    selfmade lexem dictionary in tsearch folder

--- E13/2_fulltextsearch_with_indices.sql
+++ E13/2_fulltextsearch_with_indices.sql
@@ -46,3 +46,51 @@ Triggers:
 
 -- perform query on indexex and triggered columns thats way faster
 select docid from fulltextsearch where content_tsv_gin @@ to_tsquery('mistress');
+
+-- perform query with rank
+SELECT id, left(content,40), ts_rank_cd(content_tsv_gin, query) as rank, ts_rank_cd(content_tsv_gin, query, 32 /* rank/(rank+1)
+*/) AS normalized_rank FROM FullTextSearch, to_tsquery('wonderful | good'
+) query WHERE query @@ content_tsv_gin
+ORDER BY rank DESC LIMIT 10;
+/*
++------+------------------------------------------+--------+-------------------+
+|   id | left                                     |   rank |   normalized_rank |
+|------+------------------------------------------+--------+-------------------|
+| 1200 | As Good As It Gets (1997) reviewed by St |    1.8 |          0.642857 |
+|   74 | Batman Forever (1995) reviewed by Andy J |    1.4 |          0.583333 |
+|  431 | Wonder Boys (2000) reviewed by Christoph |    1.1 |          0.52381  |
+| 1518 | People Under the Stairs, The (1992) revi |    1.1 |          0.52381  |
+|  521 | Star Trek: Insurrection (1998) reviewed  |    1   |          0.5      |
+| 1349 | Good Will Hunting (1997) reviewed by Jas |    1   |          0.5      |
+|  186 | Dogma (1999) reviewed by Shay Casey ***  |    1   |          0.5      |
+| 1305 | Go (1999) reviewed by DeWyNGaLe GO by De |    0.9 |          0.473684 |
+|  359 | Birdcage, The (1996) reviewed by Will Fi |    0.8 |          0.444444 |
+| 1439 | Elvira, Mistress of the Dark (1988) revi |    0.8 |          0.444444 |
++------+------------------------------------------+--------+-------------------+
+*/
+
+
+-- create own dictionary (with postgres.app from matt installed)
+-- cd /Applications/Postgres.app/Contents/Versions/9.4/share/postgresql/tsearch_data
+-- echo temp > synonym_movies.syn
+-- nano synonym_movies.syn #add lines of http://wiki.hsr.ch/Datenbanken/files/synonym_movies.syn
+-- ctrl+x, ctrl+y to save
+
+ -- Dictionary 'objekt' synonym_movies anlegen:
+  CREATE TEXT SEARCH DICTIONARY synonym_movies (
+    TEMPLATE = synonym,
+    SYNONYMS = synonym_movies
+  );
+  -- Dictionary 'objekt' synonym_movies der Konfiguration übergeben:
+  ALTER TEXT SEARCH CONFIGURATION english 
+    ALTER MAPPING FOR asciiword, asciihword, hword
+    WITH synonym_movies, english_stem;
+
+SELECT * FROM ts_debug('english', 'Paris');
+/*
++-----------+-----------------+---------+-------------------------------+----------------+------------+
+| alias     | description     | token   | dictionaries                  | dictionary     | lexemes    |
+|-----------+-----------------+---------+-------------------------------+----------------+------------|
+| asciiword | Word, all ASCII | Paris   | {synonym_movies,english_stem} | synonym_movies | [u'paris'] |
++-----------+-----------------+---------+-------------------------------+----------------+------------+
+*/
